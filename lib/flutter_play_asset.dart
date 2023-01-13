b070ccc8d9357e12c78eee7463d8cf024130027d
Josh Knowles <59399+chasing@users.noreply.github.com>
2023-01-13 11:55:49 -0600

    late

--- lib/flutter_play_asset.dart
+++ lib/flutter_play_asset.dart
@@ -7,20 +7,20 @@ class FlutterPlayAsset {
   final String METHOD_DOWNLOAD_PROGRESS_UPDATE = "playasset_download_pprogress_update";
   final String METHOD_GETASSET = "get_asset";
   static const platform = MethodChannel('basictomodular/downloadservice');
-  ViewPlayAsset view;
+  late ViewPlayAsset view;
 
-  init(ViewPlayAsset vw){
+  init(ViewPlayAsset vw) {
     print("JOSHX Hello, world. Again.");
     view = vw;
     platform.setMethodCallHandler((call) {
       print('platform channel method call ${call.method} ${call.arguments}');
-      if (call.method==METHOD_PLAYASSET){
-        if (!call.arguments.toString().contains("...")){
+      if (call.method == METHOD_PLAYASSET) {
+        if (!call.arguments.toString().contains("...")) {
           view.OnAssetPathFound(call.arguments.toString());
         } else {
           view.OnProcessLoadingAssetPath(call.arguments.toString());
         }
-      } else if (call.method==METHOD_DOWNLOAD_PROGRESS_UPDATE){
+      } else if (call.method == METHOD_DOWNLOAD_PROGRESS_UPDATE) {
         view.OnProgressDownload(call.arguments);
       }
     });
@@ -29,19 +29,12 @@ class FlutterPlayAsset {
   getAssetPath(String name) async {
     await platform.invokeMethod<String>(METHOD_GETASSET, name);
   }
-  
 }
 
 class ViewPlayAsset {
-  void OnProgressDownload(int percentage){
+  void OnProgressDownload(int percentage) {}
 
-  }
+  void OnAssetPathFound(String path) {}
 
-  void OnAssetPathFound(String path){
-
-  }
-
-  void OnProcessLoadingAssetPath(String path){
-
-  }
+  void OnProcessLoadingAssetPath(String path) {}
 }
